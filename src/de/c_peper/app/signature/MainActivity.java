09aace84ce5b9bdb44b8fb1ca512bba7f13010e9
Jason Peper <trublu@c-peper.de>
2014-09-30 20:32:40 +0200

    learning stuff

--- src/de/c_peper/app/signature/MainActivity.java
+++ src/de/c_peper/app/signature/MainActivity.java
@@ -1,24 +1,22 @@
 package de.c_peper.app.signature;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-public class ShowSignature extends Activity {
+public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_show_signature);
+		setContentView(R.layout.activity_main);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.show_signature, menu);
+		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
@@ -33,12 +31,4 @@ public class ShowSignature extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	
-	public void nextSignature(View view)  {
-		Intent intent = new Intent(this, MainActivity.class);
-		startActivity(intent);
-	}
-	
-	
-	
 }
